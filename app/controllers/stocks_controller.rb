cc6c3a8144bcb2e6f4f1beb927157df1d08f1108
maliksh7 <p176137@nu.edu.pk>
2022-08-15 13:22:41 +0500

    resolved errors and refactored the code

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,21 +1,16 @@
 class StocksController < ApplicationController
     def search
         #chking if some thing is entered or not
-        if params[:stock].present?
+        if params[:stock].blank?
+            flash.now[:danger] = "You have entered an empty search string"
+        else
             @stock =  Stock.new_from_lookup(params[:stock])
-            # chking if correct string is entered or not
-            if @stock
-                # render partial: 'users/results'
-                respond_to do |format|
-                    format.js { render partial: 'users/results' }
-                end
-            else
-                flash[:warning] = "You have entered an incorrect search string"
-                redirect_to my_portfolio_path
-            end
-        else 
-            flash[:danger] = "You have entered an empty search string"
-            redirect_to my_portfolio_path
+            flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
+        end
+        respond_to do |format|
+            format.js { render partial: 'users/results' }
         end
     end
+
+
 end
